a16904aa47a6c183ed00dd07c2de965b821b77b5
stanley-roache <stan.roache@gmail.com>
2018-09-11 09:12:09 +1200

    add redux store to state index

--- src/state/index.js
+++ src/state/index.js
@@ -2,5 +2,13 @@ import { SQUARE_CLICKED } from './constants'
 import { squareClicked } from './actions'
 import { initialState, rootReducer } from './reducers'
 import { getMoves } from './selectors'
+import configureStore from './store'
 
-export { getMoves, initialState, rootReducer, squareClicked, SQUARE_CLICKED }
+export {
+  configureStore,
+  getMoves,
+  initialState,
+  rootReducer,
+  squareClicked,
+  SQUARE_CLICKED
+}
---
+++ src/state/store/index.js
@@ -0,0 +1,8 @@
+// src/state/store/index.js
+import { createStore } from 'redux'
+
+import { rootReducer } from '..'
+
+export default function configureStore () {
+  return createStore(rootReducer)
+}
