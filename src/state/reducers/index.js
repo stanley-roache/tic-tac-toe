ea3eafb451b0205411dffbb262785d1596123f0c
stanley-roache <stan.roache@gmail.com>
2018-09-12 10:10:51 +1200

    updated rootReducer to handle gameOver

--- src/state/reducers/index.js
+++ src/state/reducers/index.js
@@ -1,11 +1,19 @@
 import { isUndefined } from 'ramda-adjunct'
 
-import { SQUARE_CLICKED } from '..'
+import { GAME_OVER, SQUARE_CLICKED } from '..'
 
 const initialState = { moves: [] }
 
 function rootReducer (state = initialState, { payload = {}, type }) {
+  const { square, winners: { squares, player } = {} } = payload
+
   switch (type) {
+    case GAME_OVER:
+      return {
+        ...state,
+        winningSquares: squares,
+        winningPlayer: player
+      }
     case SQUARE_CLICKED:
       return {
         ...state,
